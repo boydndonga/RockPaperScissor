94656b2ffafe4cb72792e314c25996676bd655ad
boydndonga <boydbugle@gmal.com>
2019-11-08 09:23:16 +0300

    create player two logic

---
+++ src/main/java/models/PlayerTwoImpl.java
@@ -0,0 +1,55 @@
+package models;
+
+import interfaces.PlayerInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerTwoImpl extends GameRules implements PlayerInterface {
+
+    private List<String> playStore= new ArrayList<>();
+    private String playerName;
+    private String victoryQuote;
+    private String roll;
+
+
+    public PlayerTwoImpl(String playerName, String victoryQuote) {
+        super(playerName, victoryQuote);
+        setVictoryQuote(victoryQuote);
+    }
+
+
+    @Override
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    @Override
+    public String getVictoryQuote() {
+        return victoryQuote;
+    }
+
+    public void setVictoryQuote(String victoryQuote) {
+        this.victoryQuote = victoryQuote;
+    }
+
+    @Override
+    public void setRoll(String roll) {
+        this.roll=roll;
+        this.playStore.add(roll);
+    }
+
+    @Override
+    public String getRoll() {
+        return roll;
+    }
+
+    public List<String> getPlayStore() {
+        return playStore;
+    }
+
+}
