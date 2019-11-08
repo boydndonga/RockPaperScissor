e95e8f3b07f34317ad7f6b9259a6f9da70b76526
boydndonga <boydbugle@gmal.com>
2019-11-08 14:40:19 +0300

    finish implementation of RPC game

--- src/main/java/models/Winner.java
+++ src/main/java/models/Winner.java
@@ -7,15 +7,18 @@ public class Winner {
     private String winnerRoll;
 
     public void getWin(String roll, String roll2){
-        if((roll.equals("rock")) && (roll2.equals("paper"))) {
+        if((roll.equals("rock")) && (roll2.equals("paper")) || (roll.equals("scissor")) && (roll2.equals("paper")))
+        {
             setWinner("player2");
             setWinnerRoll(roll2);
         }
-        else if ((roll.equals("rock")) && (roll2.equals("scissor"))){
+        else if ((roll.equals("rock")) && (roll2.equals("scissor")) || (roll.equals("paper")) && (roll2.equals("rock")))
+        {
             setWinner("player1");
             setWinnerRoll(roll);
         }
-        else if((roll.equals("paper")) && (roll2.equals("scissor"))){
+        else if((roll.equals("paper")) && (roll2.equals("scissor")) || (roll.equals("scissor")) && (roll2.equals("rock")
+        )){
             setWinner("player2");
             setWinnerRoll(roll2);
         }
